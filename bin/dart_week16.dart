c4fd241b40f4936560976bdfef8841451d3d270a
sarin013 <sakrinthr240145@.com>
2023-08-29 13:09:41 +0700

    first commit

---
+++ bin/dart_week16.dart
@@ -0,0 +1,12 @@
+import 'package:dart_week_16/call_fun.dart';
+import 'package:dart_week_16/return_fun.dart';
+
+
+void main(){
+  prototype();
+}
+
+void prototype(){
+  print(retrunFun("Elon Musck",45));
+  callname("Mr.Mark Zuckerberg",45);
+}
